cf914c9e18d31c5ed2b15fc5ee0318bec828c6cf
Dadzial <duszakdamian034@gmail.com>
2025-10-26 11:38:42 +0100

    add open cv

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <QApplication>
 #include "header files/MainWindow.h"
-
+#include <opencv2/opencv.hpp>
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+
+    std::cout << "OpenCV version: " << CV_VERSION << std::endl;
+
 
     MainWindow mainWindow;
     mainWindow.show();
