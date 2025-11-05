699cd6cbd3225541c92b4a788221d8cb726a9200
Dadzial <duszakdamian034@gmail.com>
2025-11-05 18:07:32 +0100

    Sender include

---
+++ src/opencv/header files/SenderOpenCV.h
@@ -0,0 +1,14 @@
+
+
+#ifndef SENDEROPENCV_H
+#define SENDEROPENCV_H
+
+
+
+class SenderOpenCV {
+
+};
+
+
+
+#endif //SENDEROPENCV_H
---
+++ src/opencv/source files/SenderOpenCV.cpp
@@ -0,0 +1,5 @@
+//
+// Created by damiand on 05.11.25.
+//
+
+#include "../header files/SenderOpenCV.h"
