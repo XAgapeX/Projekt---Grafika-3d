6331471a1a5bcdf9d42415a40cbedc164812ee4a
Katarzyna M <katarzyna.mierzynska2023@gmail.com>
2025-11-05 17:12:26 +0100

    Naprawa błędów (Windows)

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,11 @@
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+    qDebug() << "Qt plugin paths:" << QCoreApplication::libraryPaths();
+    qputenv("QT_DEBUG_PLUGINS", "1");
+    QCoreApplication::addLibraryPath(QCoreApplication::applicationDirPath() + "/plugins");
 
     std::cout << "OpenCV version: " << CV_VERSION << std::endl;
-
 
     MainWindow mainWindow;
     mainWindow.show();
